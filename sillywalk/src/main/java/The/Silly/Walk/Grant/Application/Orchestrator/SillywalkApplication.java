d135bd340d80c1a7bfc3d3aabafcf84aef292436
Lokesh <lokesh.sharma@nagra.com>
2025-10-31 14:06:34 +0530

    copilot_sillywalk

---
+++ sillywalk/src/main/java/The/Silly/Walk/Grant/Application/Orchestrator/SillywalkApplication.java
@@ -0,0 +1,13 @@
+package The.Silly.Walk.Grant.Application.Orchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SillywalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SillywalkApplication.class, args);
+	}
+
+}
---
+++ sillywalk/src/test/java/The/Silly/Walk/Grant/Application/Orchestrator/SillywalkApplicationTests.java
@@ -0,0 +1,13 @@
+package The.Silly.Walk.Grant.Application.Orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SillywalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
